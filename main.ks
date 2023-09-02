292e420468c7cddf30c08a7537abd6283da45b3e
KamikottoGG <123205307+KamikottoGG@users.noreply.github.com>
2023-09-02 23:17:02 +0300

    initial 3

--- main.ks
+++ main.ks
@@ -1,10 +1,22 @@
-name = "hello"
-if name == "hello" ->
-    print("privet")
+
+connect disnake
+where disnake.ext connect commands
+
+bot = commands.Bot(intents=disnake.Intents.all(), command_prefix="None")
+
+@bot.event
+async block on_ready() ->
+    print("i ready sucka!")
+    variable = empty
+    print(variable)
+    if variable is "None" ->
+        print("not print")
     <-
-soif name == "aboba" ->
-    print("asdad")
+    so ->
+        variable = false
     <-
-so ->
-    print("ahahha")
-    <-
+    variable = <!> variable
+    print(variable)
+<-
+
+bot.run("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")
--- src/main/java/org/example/Modules/Interperter.java
+++ src/main/java/org/example/Modules/Interperter.java
@@ -1,34 +1,28 @@
 package org.example.Modules;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Interperter {
     private File newFile;
-    private FileWriter fileWriter;
     private List<String> newCode = new ArrayList<>();
     private boolean addToCodeIndent = false;
+    private int indentLevel = 0;
 
-    public void start(List<String> lines, File file)
-    {
+    public void start(List<String> lines, File file) {
         createAndSafeFile(file);
-        createFileWriter();
-        for (String line : lines)
-        {
+
+        for (String line : lines) {
             checkKeyWords(line);
         }
-        try {
-            fileWriter.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
         writeToFile();
+        run();
     }
-    private void createAndSafeFile(File file)
-    {
+
+    private void createAndSafeFile(File file) {
         File newFile = new File(file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - 3) + ".py");
         if (!newFile.exists()) {
             try {
@@ -40,49 +34,169 @@ public class Interperter {
         this.newFile = newFile;
     }
 
-    private void createFileWriter()
-    {
-        try {
-            fileWriter = new FileWriter(newFile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    private void checkKeyWords(String line)
-    {
-        String[] words = line.split(" ");
-        if (addToCodeIndent) newCode.add("    ");
-        for (String word : words)
-        {
-            switch (word){
-                case "so" -> {
-                    newCode.add("else");
-                }
-                case "soif" ->{
-                    newCode.add("elif");
-                }
-                case "->" ->{
-                    newCode.add(":");
-                    addToCodeIndent = true;
 
+    private void checkKeyWords(String line) {
+        String[] words = line.split(" ");
+
+        StringBuilder indentBuilder = new StringBuilder();
+        Stream.generate(()-> " ").limit(indentLevel * 4L).forEach(indentBuilder::append);
+        newCode.add(indentBuilder.toString());
+
+
+        StringBuilder lineBuilder = new StringBuilder();
+        if (addToCodeIndent) {
+            lineBuilder.append(indentBuilder);
+        }
+
+        for (String word : words) {
+            switch (word) {
+                case "so" -> {
+                    lineBuilder.append("else ");
+                }
+                case "soif" -> {
+                    lineBuilder.append("elif ");
+                }
+                case "->" -> {
+                    indentLevel++;
+                    lineBuilder.append(": ");
+                    addToCodeIndent = true;
                 }
                 case "<-" -> {
+                    indentLevel--;
                     addToCodeIndent = false;
                 }
+                case "empty" -> {
+                    lineBuilder.append("None ");
+                }
+                case "pack" -> {
+                    lineBuilder.append("class ");
+                }
+                case "connect" -> {
+                    lineBuilder.append("import ");
+                }
+                case "block" -> {
+                    lineBuilder.append("def ");
+                }
+                case "false" ->{
+                    lineBuilder.append("False ");
+                }
+                case "true" -> {
+                    lineBuilder.append("True");
+
+                }
+                case "check" ->{
+                    lineBuilder.append("assert ");
+                }
+                case "like" ->{
+                    lineBuilder.append("as ");
+                }
+                case "use" ->{
+                    lineBuilder.append("with ");
+                }
+                case "stop" ->{
+                    lineBuilder.append("break ");
+                }
+                case "go" ->{
+                    lineBuilder.append("continue ");
+                }
+                case "forgot"->{
+                    lineBuilder.append("del ");
+                }
+                case "every"->{
+                    lineBuilder.append("for ");
+                }
+                case "give"->{
+                    lineBuilder.append("return ");
+                }
+                case "during"->{
+                    lineBuilder.append("while ");
+                }
+                case "generate" ->{
+                    lineBuilder.append("yield ");
+                }
+                case "attempt" -> {
+                    lineBuilder.append("try ");
+                }
+                case "fail" -> {
+                    lineBuilder.append("except ");
+                }
+                case "lastly" ->{
+                    lineBuilder.append("finally ");
+                }
+                case "anon" -> {
+                    lineBuilder.append("lambda ");
+                }
+                case "where" -> {
+                    lineBuilder.append("from ");
+                }
+                case "also" ->
+                {
+                    lineBuilder.append("and ");
+                }
+                case "else" ->{
+                    lineBuilder.append("or ");
+                }
+                case "absolute" -> {
+                    lineBuilder.append("global ");
+                }
+                case "outer" -> {
+                    lineBuilder.append("nonlocal ");
+                }
+                case "<!>" -> {
+                    lineBuilder.append("not ");
+                }
+                case "error" ->{
+                    lineBuilder.append("raise ");
+                }
+                case "equals" ->{
+                    lineBuilder.append("is ");
+                }
+                case "has" ->{
+                    lineBuilder.append("in ");
+                }
                 default -> {
-                    newCode.add(" ");
-                    newCode.add(word);
+                    lineBuilder.append(word).append(" ");
                 }
             }
         }
 
+        newCode.add(lineBuilder.toString().trim());
         newCode.add("\n");
+
     }
-    private void writeToFile()
+    public void run()
     {
-        for(String code : newCode)
-        {
-            System.out.print(code);
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("python", newFile.getAbsolutePath());
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("Код завершения: " + exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private void writeToFile() {
+
+        try (FileWriter fileWriter = new FileWriter(newFile);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);) {
+            for(String code : newCode)
+            {
+                bufferedWriter.write(code );
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
